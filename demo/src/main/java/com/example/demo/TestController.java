07017918e4881bce7c716b753dd59272db4deb5a
unknown <ajinkya.hurbade@gmail.com>
2026-05-17 22:27:18 +0530

    fix

--- demo/src/main/java/com/example/demo/TestController.java
+++ demo/src/main/java/com/example/demo/TestController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class TestController
 {
 
-    @GetMapping("/get")
+    @GetMapping("/getMsg")
     public String getMessage(){
         return "SuccessFully Set Up";
     }
